3f765da5203d929b34a12875710afb91e7524e0d
Mohit Malik <mohit.malik@chicmic.co.in>
2022-11-15 11:31:28 +0530

    initial commit

---
+++ assets/Scripts/resourceLoader.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('resourceLoader')
+export class resourceLoader extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
