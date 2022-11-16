22e6b79bb081a18cb8bb0abf39e87bc249e9d16e
Mohit Malik <mohit.malik@chicmic.co.in>
2022-11-16 18:31:58 +0530

    setting the scale of the images inside scollView

--- assets/Scripts/imageLoad.ts
+++ assets/Scripts/imageLoad.ts
@@ -14,8 +14,15 @@ export class imageLoad extends Component {
         items.forEach((item) => {
             let ite = instantiate(this.image);
             ite.getComponent(Sprite).spriteFrame = item;
-            ite.getComponent(UITransform).height = 150;
-            ite.getComponent(UITransform).width = 180;
+
+            //height and width of images
+            //let height=ite.getComponent(UITransform).height 
+            let pwidth = this.contentNode.getComponent(UITransform).width
+            let cwidth= ite.getComponent(UITransform).width
+            // let scaleFactor = 1/Math.max(cwidth/pwidth);
+            // ite.setScale(Math.min(scaleFactor, 1));
+            ite.getComponent(UITransform).width= (this.contentNode.getComponent(UITransform).width)/2;
+            ite.getComponent(UITransform).height = (this.contentNode.getComponent(UITransform).width)/2
             this.contentNode.addChild(ite);
         });
     }
