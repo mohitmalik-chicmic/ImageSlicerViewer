81cdc959a0f71ac7db726abd5e21c8c027576cc2
= <=>
2022-11-21 17:24:07 +0530

    result layer integration

--- assets/Scripts/GamePlay.ts
+++ assets/Scripts/GamePlay.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, ImageAsset, SpriteFrame, Texture2D, math, Sprite, macro, Prefab, instantiate, Label, Enum, CCInteger } from 'cc';
+import { _decorator, Component, Node, ImageAsset, SpriteFrame, Texture2D, math, Sprite, macro, Prefab, instantiate, Label, Enum, CCInteger, UITransform, Vec3 } from 'cc';
 
 import { photoSlice2 } from './photoSlice2';
 
@@ -14,49 +14,50 @@ export class GamePlay extends Component {
     photo : Prefab = null;
     
     @property({type: CCInteger})
-    spilt : any = 0;
-
-   
-
+    sliceNumber : any = 0;
     start() {
-        // let image=new SpriteFrame();
-        // let texture=new Texture2D();
-        // texture.image=this.imageAssert;
-        // image.texture=texture;
-        // let rect=math.rect(0,0,image.width,200);
-        // image.setRect(rect);
-        // this.node.getComponent(Sprite).spriteFrame=image;
-        var a= this.node.getComponent(GamePlay).spilt
-        console.log(typeof(this.node.getComponent(GamePlay).spilt));
+    
+        var a=new Array();
+       
+        var numOfSlice= this.node.getComponent(GamePlay).sliceNumber;
         
         let image=new SpriteFrame();
         let texture=new Texture2D();
         texture.image=this.imageAssert;
         image.texture=texture;
-        // var h=518/a;
-        var b=0;
-        while(b<a){
+        var loopNum=0;
+        while(loopNum<numOfSlice){
             let smallSlice=instantiate(this.photo);
             let smallSliceClass=smallSlice.getComponent(photoSlice2);
-            // let rect=math.rect(0+image.height/this.num,0+image.height/this.num,image.width,image.height/this.num);
-            smallSliceClass.setSlice(a,b);
+            smallSliceClass.setSlice(numOfSlice,loopNum);
             this.node.addChild(smallSlice);
-            // Node.EventType.TOUCH_MOVE();
-            // smallSlice.on(Node.EventType.TOUCH_MOVE, this.onTouchStartCallback, this, true);
+            console.log(smallSlice.name);
             let pos = smallSlice.getPosition();
-            pos.y+=259-b*(10+(image.height/a));
-            console.log(pos.x,pos.y);
+            let radomH=this.fgetRandom(a,0,numOfSlice);
+            if(a[radomH]!=undefined){
+            for(let i=0;i<numOfSlice;i++){
+                if(a[i]==undefined){
+                    radomH=i;
+                    a[i]=1;
+                    break;
+                }
+            }
+            }else{
+                a[radomH]=1;
+            }
+          
+            pos.y=(this.node.getComponent(UITransform).height/2)-(image.height/numOfSlice)-radomH*(5+(image.height/numOfSlice));
             smallSlice.setPosition(pos);
-            b++;
+            loopNum++;
         } 
-        
-
     }
-    // onTouchStartCallback (event){
-    //     console.log(event);
-
-    // }
-
+    
+    fgetRandom(a,min, max) {
+        var flag =true;
+        let value=Math.floor(Math.random() * (max - min) + min); 
+        return value;
+        
+      }
     update(deltaTime: number) {
         
     }
--- assets/Scripts/imageLoad.ts
+++ assets/Scripts/imageLoad.ts
@@ -1,31 +1,31 @@
-import { _decorator, Component, Node, Prefab, instantiate, SpriteFrame, Sprite, UITransform } from "cc";
-const { ccclass, property } = _decorator;
+// import { _decorator, Component, Node, Prefab, instantiate, SpriteFrame, Sprite, UITransform } from "cc";
+// const { ccclass, property } = _decorator;
 
-@ccclass("imageLoad")
-export class imageLoad extends Component {
-    @property({ type: Prefab })
-    image: Prefab = null!;
+// @ccclass("imageLoad")
+// export class imageLoad extends Component {
+//     @property({ type: Prefab })
+//     image: Prefab = null!;
 
-    @property({ type: Node })
-    contentNode: Node = null;
+//     @property({ type: Node })
+//     contentNode: Node = null;
 
-    start() {}
-    show(items: SpriteFrame[]) {
-        items.forEach((item) => {
-            let ite = instantiate(this.image);
-            ite.getComponent(Sprite).spriteFrame = item;
+//     start() {}
+//     show(items: SpriteFrame[]) {
+//         items.forEach((item) => {
+//             let ite = instantiate(this.image);
+//             ite.getComponent(Sprite).spriteFrame = item;
 
-            //height and width of images
-            //let height=ite.getComponent(UITransform).height 
-            let pwidth = this.contentNode.getComponent(UITransform).width/2
-            let cwidth= ite.getComponent(UITransform).width
-            ite.getComponent(UITransform).height= (ite.getComponent(UITransform).height)*(pwidth/cwidth)
+//             //height and width of images
+//             //let height=ite.getComponent(UITransform).height 
+//             let pwidth = this.contentNode.getComponent(UITransform).width/2
+//             let cwidth= ite.getComponent(UITransform).width
+//             ite.getComponent(UITransform).height= (ite.getComponent(UITransform).height)*(pwidth/cwidth)
 
-            ite.getComponent(UITransform).width= (this.contentNode.getComponent(UITransform).width)/2;
-           // ite.getComponent(UITransform).height = (this.contentNode.getComponent(UITransform).width)/2
-            this.contentNode.addChild(ite);
-        });
-    }
+//             ite.getComponent(UITransform).width= (this.contentNode.getComponent(UITransform).width)/2;
+//            // ite.getComponent(UITransform).height = (this.contentNode.getComponent(UITransform).width)/2
+//             this.contentNode.addChild(ite);
+//         });
+//     }
 
-    update(deltaTime: number) {}
-}
+//     update(deltaTime: number) {}
+// }
